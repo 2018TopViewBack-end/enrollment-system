58319d04347f38ee1c62bd363715148bb5e024fc
no one <L827912244@163.com>
2018-08-13 23:00:10 +0800

    web.xml新增编码过滤器,实现stage的增删查改

--- src/test/java/org/topview/controller/department/StageControllerTest.java
+++ src/test/java/org/topview/controller/department/StageControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.topview.entity.department.po.Stage;
 import org.topview.service.department.StageService;
 import org.topview.util.Result;
